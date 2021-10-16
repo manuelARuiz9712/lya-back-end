78d375ff5dc1b571329e769db8bd9fd09683ccbd
=Manuel angulo <manuelanguloruiz@gmail.com>
2021-10-15 22:57:43 -0500

    adding swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './modules/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+
+  const config = new DocumentBuilder()
+  .setTitle('Lya electronic test')
+  .setDescription('Wonderfull test with nest.js')
+  .setVersion('1.0')
+  .addTag('lyatest')
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, document);
+
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3500);
